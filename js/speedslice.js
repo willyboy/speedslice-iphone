e69762184c158e759fa352ced8f64e6348734981
William Neely <william@piecewise.com>
2013-04-03 20:29:25 -0400

    Splash screens

--- js/speedslice.js
+++ js/speedslice.js
@@ -122,6 +122,7 @@ function loadInfo(){
 		setTimeout("dontFocus=false",400);
 	});
 	$("#addPizza.ribbon").on("touchstart",function(){
+		$("body").prepend("1<br>");
 		//fix bug where pizza can have same name and different toppings
 		thePiz=$("#pizzaName");
 		//ie
@@ -184,7 +185,7 @@ function loadInfo(){
 			notLoggedInToppings=notLoggedInToppings.substr(0,notLoggedInToppings.length-1);
 			$("#pizzaID").append("<option data-toppings='"+notLoggedInToppings+"'>"+$("#pizzaName").val()+"</option>");
 		}*/
-		checkCustomScrolling();
+		/*checkCustomScrolling();*/
 	});
 	$("#tapOrder").on("touchstart",function(){
 		orderPizzaPage();
@@ -325,7 +326,7 @@ function getDeliveryOpts(){
 			$.each(data,function(index,value){
 				$("#delOpts").append('<div class="next bigRed delLoc"><div class="editButton">EDIT</div>'+value+'</div>');
 			});
-			checkCustomScrolling();
+			/*checkCustomScrolling();*/
 		}
 	});
 }
@@ -418,7 +419,7 @@ function orderPizzaPage(curSlide){
 				$.each(data,function(index,value){
 					$("#orderOptions").append("<div><h4 class='orderOpt' data-order='"+value.Tray_Order+"' data-restID='"+value.RestaurantID+"'>"+value.Rest_Name+"<span class='fR pl10'>$"+value.Total_Price+"</span></h4></div>");
 				});
-				checkCustomScrolling();
+				/*checkCustomScrolling();*/
 			}
 			else{
 				$("#orderOptions").append("<div><h4 id='noRests'>"+data.error+"</h4></div>");
